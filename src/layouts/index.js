b5cac4832122ae41bfa1e63744186b8745efdab4
Tom Longmate <tomlongmate@gmail.com>
2018-01-30 18:46:53 +0000

    add  text

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -6,13 +6,13 @@ import './semantic.css';
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet
-      title="TFL Green Card: reclaim Your City"
+      title="TFL Green Card: Keeping London Active"
       meta={[
         { name: 'description', content: 'Sample' },
         { name: 'keywords', content: 'sample, something' },
         { name: 'viewport', content: 'width = device-width, initial-scale = 1.0, minimum-scale = 1, maximum-scale = 1, user-scalable = no'},
         { name: 'apple-mobile-web-app-capable', content: 'yes'},
-        { name: 'apple-touch-icon', sizes: '144x144', content: 'img/apple-icon-144x144.png'},
+        { name: 'apple-touch-icon', sizes: '144x144', href: 'apple-touch-icon.png'},
       ]}
     />
     <div>
